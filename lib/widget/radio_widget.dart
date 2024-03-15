b147f7eef29fe57625d2de1e0a0d1f8270f6f487
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-03-15 19:04:33 +0700

    create radio button for gender

---
+++ lib/widget/radio_widget.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class RadioWidget extends StatefulWidget {
+  const RadioWidget({super.key});
+
+  @override
+  State<RadioWidget> createState() => _RadioWidgetState();
+}
+
+List<String> _radioList = ["Laki-laki", "Perempuan"];
+
+class _RadioWidgetState extends State<RadioWidget> {
+  String currentOption = _radioList[0];
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text(
+          "Masukan Jenis Kelamin",
+          style: TextStyle(fontSize: 15),
+        ),
+        ListTile(
+          title: Text('Laki-Laki'),
+          leading: Radio(
+            value: _radioList[0],
+            groupValue: currentOption,
+            onChanged: (value) {
+              setState(() {
+                currentOption = value.toString();
+              });
+            },
+          ),
+        ),
+        ListTile(
+          title: Text('Perempuan'),
+          leading: Radio(
+              value: _radioList[1],
+              groupValue: currentOption,
+              onChanged: (value) {
+                setState(() {
+                  currentOption = value.toString();
+                });
+              }),
+        )
+      ],
+    );
+  }
+}

fab3fb2cbd0d4940bfeb16380bf2a8f0a1e3258d
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-03-15 21:25:08 +0700

    make data class for parsing data

---
+++ lib/data/Order.dart
@@ -0,0 +1,14 @@
+class Order {
+  final String nama;
+  final String notelp;
+  final String makanan;
+  final String minuman;
+  final String dessert;
+  Order({
+    required this.nama,
+    required this.notelp,
+    required this.makanan,
+    required this.minuman,
+    required this.dessert,
+  });
+}
